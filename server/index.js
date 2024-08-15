7c1f85f2f3dad0c5fd47ecd6096dee305d9cfa49
RAJA <141742767+raja2825@users.noreply.github.com>
2024-08-15 16:56:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const EmployeeModel=require('./models/Employee')
 const app=express()
-app.use(cors({
-    origin:["https://deploy-mern-1whq.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-}))
+// app.use(cors({
+//     // origin:["https://deploy-mern-1whq.vercel.app"],
+//     // methods:["POST","GET"],
+//     // credentials:true
+// }))
+app.use(cors())
+
 app.use(express.json())
 mongoose.connect("mongodb+srv://rajasankar2004s:<!EUqFH@h$Jqm7j$>@cluster0.3apgo.mongodb.net/employee?retryWrites=true&w=majority&appName=Cluster0");
 
